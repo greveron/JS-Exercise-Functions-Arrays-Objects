afd59361d209a1b767a45ca559a9240d88ef8f6d
greveron.Germain <reverong@gmail.com>
2019-12-06 22:42:54 -0500

    JS1-Germain-Reveron

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  const person = {
+  id:id,
+  name:name,
+  email:email,
+}
+return person;
+  
 }
 
 /**
@@ -56,7 +62,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
+function getName(argument) {
+  return 'Hello, my name is' + argument.name;
   /* code here */
 }
 
@@ -73,8 +80,14 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  const object = {
+    name:name,
+    sum : (num1, num2) => (num1 + num2),
+    return 'Hello my name is ' + Object.name,
+
+  }
+
 }
 
 
@@ -136,6 +149,8 @@ function get3rdCar(inventory) {
  * it will return `This is a Lincoln Navigator`.
 */
 function getCarInfoByIndex(inventory, index) {
+  var getCarInfoByIndex = inventory.find (item,index)
+  
   /* code here */
 }
 
